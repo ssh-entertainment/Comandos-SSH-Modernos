7dd70d6a43290d1d096435762f587cb8df5338bd
Alejandro Rodríguez Rojas <alexrodriguezrojas98@gmail.com>
2019-02-20 10:10:12 +0100

    apt-get install python3-paramiko para que funcione(Actualizado By LuisaoEstufa)

--- Comandos.py
+++ Comandos.py
@@ -1,31 +1,51 @@
 # Librerías necesarias
 import paramiko
 import os
+
+def Comando_por_ssh(ssh_servidor,ssh_puerto,ssh_usuario,ssh_clave,comando):
+
+	# Conectamos al servidor
+	conexion = paramiko.Transport((ssh_servidor, ssh_puerto))
+	conexion.connect(username = ssh_usuario, password = ssh_clave)
  
-comando=input("¿Cual es el comando que quieres realizar? ")
-ssh_servidor=input("¿Cual es la IP? ")
-ssh_usuario=input("¿Cual es el usuario? ")
-ssh_clave=input("¿Cual es la contraseña? ")
+	# Abrimos una sesión en el servidor
+	canal = conexion.open_session()
+	# Ejecutamos el comando, en este caso un sencillo 'ls' para ver
+	# el listado de archivos y directorios
+	canal.exec_command(comando)
+
+	# Y vamos a ver la salida
+	salida = canal.makefile('rb', -1).readlines()
+	if salida:
+		# Si ha ido todo bien mostramos el listado de directorios
+		print(salida)
+	else:
+		# Si se ha producido algún error lo mostramos
+		print(canal.makefile_stderr('rb', -1).readlines())
+	conexion.close()
+
+
+comando=input("Comando:		 ")
+
 # Datos para la conexión SSH
+ssh_servidor = input("Servidor:		")
+ssh_usuario  = input("Usuario:		")
+ssh_clave    = input("Contraseña:	")
 ssh_puerto   = 22 # O el puerto SSH que use nuestro servidor
 '''comando      = 'yes hola > /dev/pts/0' '''# el comando que vamos a ejecutar en el servidor
- 
-# Conectamos al servidor
-conexion = paramiko.Transport((ssh_servidor, ssh_puerto))
-conexion.connect(username = ssh_usuario, password = ssh_clave)
- 
-# Abrimos una sesión en el servidor
-canal = conexion.open_session()
-# Ejecutamos el comando, en este caso un sencillo 'ls' para ver
-# el listado de archivos y directorios
-canal.exec_command(comando)
 
-# Y vamos a ver la salida
-salida = canal.makefile('rb', -1).readlines()
-if salida:
-	# Si ha ido todo bien mostramos el listado de directorios
-	print(salida)
-else:
-	# Si se ha producido algún error lo mostramos
-	print(canal.makefile_stderr('rb', -1).readlines())
-conexion.close()
+Comando_por_ssh(ssh_servidor,ssh_puerto,ssh_usuario,ssh_clave,comando) 
+
+while true:
+
+	Decision=input("	¿Quieres introducir otro comando?	").upper()
+	
+	if Decision=='SI':
+
+		comando=input("Comando:		 ")
+
+		Comando_por_ssh(ssh_servidor,ssh_puerto,ssh_usuario,ssh_clave,comando)
+
+	else:
+
+		break
